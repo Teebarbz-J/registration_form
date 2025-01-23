e0121b795e984317cae225500b884995fb101214
justina <justinbarbz83@gmail.com>
2025-01-23 12:39:39 +0100

    first commit

---
+++ lib/Screens/registration_screen.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends
+statefulWidget {
+  _RegistrationScreenstate createstate()
+=> _RegistrationScreenstate();
+
+class _RegistrationScreenstate extends 
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import "Screens/registration_screen.dart"
+void main () {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+@override
+  Widget build(BuildContext context ) {
+    return MaterialApp (
+     title: "Registration Form" , 
+     home: RegistrationScreen() ,
+    );
+    
+  }
+
+
+
+}
