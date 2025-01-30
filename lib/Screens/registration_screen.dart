f435757fd0dfa0f636551132766aa1971cce9bcd
justina <justinbarbz83@gmail.com>
2025-01-30 12:44:51 +0100

    registration_form

--- lib/Screens/registration_screen.dart
+++ lib/Screens/registration_screen.dart
@@ -1,68 +1,103 @@
 
-
 import 'package:flutter/material.dart';
+import 'package:registration_form/screens/details_screen.dart';
 
-class  RegistrationScreen extends StatefulWidget{
-  _RegistrationScreenState createState() => _RegistrationScreenState();
+
+class  RegistrationScreen extends 
+StatefulWidget{
+  @override
+_RegistrationScreenState createState() => _RegistrationScreenState();
 }
-class _RegistrationScreenState extends State<RegistrationScreen>{
-  final TextEditingController firstNameController= TextEditingController();
-  final  TextEditingController lastNameController= TextEditingController();
-  final  TextEditingController emailController= TextEditingController();
-  bool isRegistered=false;
 
-  @override  
-Widget build(BuildContext context){
+class _RegistrationScreenState extends
+State<RegistrationScreen> {
+  final TextEditingController firstNameController = TextEditingController( );
+  final TextEditingController lastNameController = TextEditingController( );
+  final TextEditingController emailController = TextEditingController( );
+  bool isRegistered = false;
+
+  @override
+Widget build (BuildContext context) {
   return Scaffold(
     appBar: AppBar(
-      title: Text("Registration Form"),
+      title:const Text("Registration form"),
     ),
-
-    body:   Padding(padding:const EdgeInsets.all(20),
-    child:  Column(mainAxisAlignment: MainAxisAlignment.
-    center,
-    children: [
-      TextField(controller: firstNameController, decoration:const InputDecoration(labelText: "First Name"),
-
-      ),
-      TextField(controller: lastNameController, decoration:const InputDecoration(labelText: "Last Name"),
-
-      ),
-      TextField(controller: emailController, decoration:const InputDecoration(labelText: "E-mail Adress"),
-
-      ),
-     const  SizedBox(height: 30,),
-     GestureDetector(onTap: () {
-       setState(() {
-         isRegistered = ! isRegistered;
-       });
-     },
-     child: Container(padding:const EdgeInsets.all(20),
-     decoration: BoxDecoration(color: isRegistered ? Colors.
-     green: Colors.blue,
-     borderRadius: BorderRadius.circular(10),
-
-
-     ),
-     child: Text(isRegistered?
-     "Registration Successful": 
-"Register Me",
-style: const TextStyle(
-  color: Colors.white,
-  fontSize: 20
+  body: Padding(padding:const EdgeInsets.all(20),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children:<Widget> [
+      TextField(
+controller: firstNameController,
+decoration: const
+InputDecoration(
+  labelText: "First Name"
 ),
-     )
-     
-     
-
-     ),
-
-     )
-    ],
-    )
+      ),
+      TextField(
+controller: lastNameController,
+decoration: const
+InputDecoration(
+  labelText: "Last Name"
+),
+  ),
+      TextField(
+controller: emailController,
+decoration: const
+InputDecoration(
+  labelText: "Email Address"
+),
+ ),
+    
+    const SizedBox(height: 30,),
+    GestureDetector(
+      onTap: () {
+        setState(() {
+          isRegistered = !isRegistered;
+        });
+      },
+      child: Container(
+padding:const EdgeInsets.all(20),
+decoration: BoxDecoration(
+  color: isRegistered ? Colors.
+  green: Colors.blue,
+  borderRadius: BorderRadius.
+  circular(10),
+),
+child:  Text(
+isRegistered ? 
+"Registration Successful" :
+"Register Me",
+style:const TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+),
+),
+      ),
     ),
+    const SizedBox(height: 30,),
+    if(isRegistered)
+    ElevatedButton(onPressed: (){
+Navigator .push(context, 
+MaterialPageRoute(builder: (context) => DetailsScreen(
+  firstname: firstNameController.text,
+  lastname: lastNameController.text,
+  email: emailController.text,
 
-  );
+)));
+
+
+    }, 
+    
+    child: const Text("View registration details")
+
+    )
+
+    
+    ],
+
+  )
+  
+  
+  ));
 }
 }
-
